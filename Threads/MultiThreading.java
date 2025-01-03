7cb1ac4ba7736ebee7fe57470350cd29e0622fe6
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2025-01-03 09:33:03 +0530

    Enhance multi-threading implementation with thread identification and exception handling

--- Threads/MultiThreading.java
+++ Threads/MultiThreading.java
@@ -1,10 +1,21 @@
 package Threads;
 
+
 public class MultiThreading extends Thread {
+
+    private int ThreadNumber;
+
+    public MultiThreading(int t) {
+        this.ThreadNumber = t;
+    }
 
     public void run() {
         for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+            System.out.println(i + " is from " + ThreadNumber);
+
+            if (ThreadNumber == 2) {
+                throw new RuntimeException();
+            }
 
             try {
                 Thread.sleep(1000);
