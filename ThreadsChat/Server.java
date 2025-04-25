417bdb2790227c4370e3b48aa0ceaf63ea7c9e44
ThashmikaX <thashmikax2gmail.com>
2025-04-25 06:54:59 +0530

    Remove unused imports in Server.java to clean up the code

--- ThreadsChat/Server.java
+++ ThreadsChat/Server.java
@@ -1,12 +1,8 @@
 package ThreadsChat;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Server {
 	static int port = 4001;
